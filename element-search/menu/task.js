41dadcc0c05e55fcbf97d8d6282b45c80ec3fbda
Viktor Bauer <bassa555@mail.ru>
2023-01-23 16:15:23 +0300

    homework menu correction

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,17 +5,14 @@ menuLink.forEach(el => {
         let activeSelect = el.parentElement.querySelector("ul");
         let activeMenu = el.closest(".menu_main").querySelector(".menu_active");
 
-        if (activeSelect.classList.contains("menu_active")) {
+        if(activeSelect.classList.contains("menu_active")) {
             activeMenu.classList.remove("menu_active");
+        } else if (activeMenu) {
+            activeMenu.classList.remove("menu_active");
+            activeSelect.classList.add("menu_active");
         } else {
-            if (activeMenu) {
-                activeMenu.classList.remove("menu_active");
-                activeSelect.classList.add("menu_active");
-            } else {
-                activeSelect.classList.add("menu_active");
-            }
+            activeSelect.classList.add("menu_active"); 
         }
         return false;
     }
 });
-
