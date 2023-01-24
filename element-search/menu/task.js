da2e3629a7d33db68a5a2491cf26d8161b3dc6c0
Viktor Bauer <bassa555@mail.ru>
2023-01-24 15:35:06 +0300

    homework menu change

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,17 +1,16 @@
 const menuLink = Array.from(document.getElementsByClassName("menu__link"));
+const menuSub = Array.from(document.getElementsByClassName("menu_sub"));
 
 menuLink.forEach(el => {
     el.onclick = function () {
-        let activeSelect = el.parentElement.querySelector("ul");
-        let activeMenu = el.closest(".menu_main").querySelector(".menu_active");
-
-        if(activeSelect.classList.contains("menu_active")) {
-            activeMenu.classList.remove("menu_active");
-        } else if (activeMenu) {
-            activeMenu.classList.remove("menu_active");
+        let activeSelect = el.nextElementSibling;
+        if(activeSelect !==null && activeSelect.classList.contains("menu_sub") && activeSelect.classList.contains("menu_active")) {
+            activeSelect.classList.remove("menu_active");
+        } else if(activeSelect !==null && activeSelect.classList.contains("menu_sub")) {
+            menuSub.forEach(el => el.classList.remove("menu_active"));
             activeSelect.classList.add("menu_active");
         } else {
-            activeSelect.classList.add("menu_active"); 
+            activeSelect.classList.add("menu_active");
         }
         return false;
     }
