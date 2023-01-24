e8d1e2fe8ddb317fdaf3ce6bf9ff9fb513c121c8
Viktor Bauer <bassa555@mail.ru>
2023-01-24 15:38:09 +0300

    homework reveal corection

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,9 +1,23 @@
 const reveal = Array.from(document.getElementsByClassName("reveal"));
 
+// document.addEventListener("scroll", function() {
+//     reveal.forEach((element) => {
+
+//         element.getBoundingClientRect().bottom < 0 || element.getBoundingClientRect().top > window.innerHeight 
+//             ? element.classList.remove("reveal_active") 
+//             : element.classList.add("reveal_active");
+//     });
+// });
+
+
 document.addEventListener("scroll", function() {
     reveal.forEach((element) => {
-        element.getBoundingClientRect().bottom < 0 || element.getBoundingClientRect().top > window.innerHeight 
-            ? element.classList.remove("reveal_active") 
-            : element.classList.add("reveal_active");
+        const { innerHeight } = window;
+        const { top, bottom } = element.getBoundingClientRect();
+        if (top < innerHeight && bottom > 0) {
+            element.classList.add("reveal_active");
+        } else {
+            element.classList.remove("reveal_active");
+        }
     });
 });
