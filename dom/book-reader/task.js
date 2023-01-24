0d8a91cb5be67661ac4b3b8ec21ebeecaa3444c1
Viktor Bauer <bassa555@mail.ru>
2023-01-24 15:41:28 +0300

    homework book-reader corection

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,34 +1,34 @@
-const book = document.getElementById("book");
+const book = document.querySelector(".book");
 const fontSize = Array.from(document.getElementsByClassName("font-size"));
-const controlColor = Array.from(document.getElementsByClassName("book__control_color").children);
-const controlBackground = Array.from(document.getElementsByClassName("book__control__background").children);
+const controlColor = Array.from(document.querySelector(".book__control_color").getElementsByClassName("color"));
+const controlBackground = Array.from(document.querySelector(".book__control_background").getElementsByClassName("color"));
 
 fontSize.forEach((el) => {
-    el.onclick = (event) => {
-        event.preventDefault();
+    el.addEventListener("click", (event) => {
         document.querySelector(".font-size_active").classList.remove("font-size_active");
         el.classList.add("font-size_active");
         book.classList.remove("book_fs-small","book_fs-big");
         book.classList.add(`book_fs-${el.getAttribute("data-size")}`);
-    }
+        event.preventDefault();
+    });
 });
 
 controlColor.forEach((el) => {
-    el.onclick = (event) => {
-        event.preventDefault();
-        el.closest(".book__control_color").querySelector(".color_active").classList.remove("color_active");
+    el.addEventListener("click", (event) => {
+        document.querySelector(".color_active").classList.remove("color_active");
         el.classList.add("color_active");
-        book.classList.remove('book_color-black', 'book_color-gray', 'book_color-whitesmoke');
-        book.classList.add(`book_color-${el,getAttribute("data-text-color")}`);
-    }
+        book.classList.remove("book_color-black", "book_color-gray", "book_color-whitesmoke");
+        book.classList.add(`book_color-${el.getAttribute("data-text-color")}`);
+        event.preventDefault();
+    });
 });
 
 controlBackground.forEach((el) => {
-    el.onclick = (event) => {
-        event.preventDefault();
-        el.closest(".book__control_background").querySelector(".color_active").classList.remove("color_active");
+    el.addEventListener("click", (event) => {
+        document.querySelector(".color_active").classList.remove("color_active");
         el.classList.add("color_active");
-        book.classList.remove('book_bg-black', 'book_bg-gray', 'book_bg-white');
-        book.classList.add(`book_color-${el,getAttribute("data-text-color")}`);
-    }
+        book.classList.remove("book_bg-black", "book_bg-gray", "book_bg-white");
+        book.classList.add(`book_bg-${el.getAttribute("data-bg-color")}`);
+        event.preventDefault();
+    });
 });
